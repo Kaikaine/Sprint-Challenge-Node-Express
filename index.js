ff54f8da0cb5f364452cb4046778107291861e82
Kairi <kairijb@gmail.com>
2018-09-07 11:00:16 -0500

    crud operations for project done

--- index.js
+++ index.js
@@ -31,15 +31,15 @@ server.get('/actions', (req, res) => {
 server.get('/actions/:id',  (req, res) => {
     const {id} = req.params;
 
-    actionDb.get(id).then(user => {
-        res.status(200).json(user)
+    actionDb.get(id).then(action => {
+        res.status(200).json(action)
     })
-        .catch(err => res.status(404).json({ message: "The user with the specified ID does not exist." }))
+        .catch(err => res.status(404).json({ message: "The action with the specified ID does not exist." }))
 })
 
 server.post('/actions', (req, res) => {
     actionDb.insert(req.body)
-        .then(response => res.status(201).json({ message: 'user creation success' }))
+        .then(response => res.status(201).json({ message: 'action creation success' }))
         .catch(err => res.status(500).json(err))
 })
 
@@ -55,11 +55,50 @@ server.put('/actions/:id', (req, res) => {
     const { id } = req.params
     actionDb.update(id, req.body)
         .then(actions => res.status(200).json(actions))
-        .catch(err => res.status(500).json({ message: 'user update fail' }))
+        .catch(err => res.status(500).json({ message: 'action update fail' }))
 })
 
 
 // project
+
+server.get('/project', (req, res) => {
+
+    projectDb.get().then(project => {
+        res.status(200).json(project)
+    })
+        .catch(err => res.status(404).json({ message: "couldnt access project" }))
+})
+
+server.get('/project/:id', (req, res) => {
+    const {id} = req.params;
+
+    projectDb.get(id).then(project => {
+        res.status(200).json(project.id)
+    })
+        .catch(err => res.status(404).json({ message: "The project with the specified ID does not exist." }))
+})
+
+server.post('/project', (req, res) => {
+    projectDb.insert(req.body)
+        .then(response => res.status(201).json({ message: 'project creation success' }))
+        .catch(err => res.status(500).json({ message: 'error creating project' }))
+})
+
+server.delete('/project/:id', (req, res) => {
+    const { id } = req.params
+
+    projectDb.remove(id)
+        .then(count => res.status(204).json({message: 'delete successful'}).end())
+        .catch(err => res.status(500).json({ message: 'delete unsuccessful' }))
+})
+
+server.put('/project/:id', (req, res) => {
+    const { id } = req.params
+    projectDb.update(id, req.body)
+        .then(project => res.status(200).json(project))
+        .catch(err => res.status(500).json({ message: 'project update fail' }))
+})
+
 
 // start the server
 server.listen(6000, () => console.log('API On Port 6000'));
